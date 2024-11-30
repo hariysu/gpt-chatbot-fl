9b6165254cdad82ba9877a1c663792fbaa11e355
Abdurahman AYDIN <hariysu@gmail.com>
2024-11-30 14:18:25 +0300

    Add gemini api consts

---
+++ lib/constants/openai_api_consts.dart
@@ -0,0 +1,3 @@
+String baseUrl = "https://api.openai.com/v1";
+String apiKey =
+    "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";
